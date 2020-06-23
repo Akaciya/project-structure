e2310db717eae08a43058b62c2b4f56170aa8e7e
Akaciya <start30@yandex.ru>
2020-06-23 12:13:54 +0300

    чистка закомментированного кода

--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,35 +1,12 @@
 import SortableTable from '../../../components/sortable-table/index.js';
 import header from './bestsellers-header.js';
 
-import fetchJson from '../../../utils/fetch-json.js';
-
 export default class Page {
   element;
   subElements = {};
   components = {};
 
-  async updateComponents () {
-    const data = await fetchJson(`${process.env.BACKEND_URL}api/rest/products?_embed=subcategory.category&_start=1&_end=20`);
-
-    this.components.sortableTable.addRows(data);
-
-    //this.components.ordersChart.update(from, to);
-    //this.components.salesChart.update(from, to);
-    //this.components.customersChart.update(from, to);
-  }
-
   async initComponents () {
-    const to = new Date();
-    const from = new Date(to.getTime() - (30 * 24 * 60 * 60 * 1000));
-/*url = '',
-    sorted = {
-      id: headersConfig.find(item => item.sortable).id,
-      order: 'asc'
-    },
-    isSortLocally = false,
-    step = 20,
-    start = 1,
-    end = start + step*/
 
     const sortableTable = new SortableTable(header, {
       url: `api/rest/products?_embed=subcategory.category`,
@@ -63,7 +40,6 @@ export default class Page {
     await this.initComponents();
 
     this.renderComponents();
-    //this.initEventListeners();
 
     return this.element;
   }
@@ -87,15 +63,7 @@ export default class Page {
     }, {});
   }
 
-  /*initEventListeners () {
-    this.components.rangePicker.element.addEventListener('date-select', event => {
-      const { from, to } = event.detail;
-
-      this.updateComponents(from, to);
-    });
-  }*/
-
-  remove () {
+   remove () {
     this.element.remove();
   }
 
